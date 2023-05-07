44c74820c6a01f78f0688a144e5edb8198b8ef3e
sanjogR <sanjog@fusemachines.com>
2023-05-07 23:31:09 +0545

    feat: electronicDanceMusic riff getRandomPattern

---
+++ index.js
@@ -0,0 +1,38 @@
+import scribbletune from 'scribbletune';
+
+const ROOT = 'B2';
+const SCALE = 'major';
+
+const getRandomPattern = function (count) {
+	let str = '[x-]R';
+	for (let i = 1; i < (count || 8); i++) {
+	  str += Math.round(Math.random()) ? '[x-]R' : 'R[x-]';
+	}
+  
+	return str;
+  }
+
+const getRiff = (note) => {
+	return scribbletune.clip({
+		notes: 'D2',
+		randomNotes: scribbletune.getChordsByProgression('D2 minor', 'ii iii'),
+		subdiv: '16n',
+		pattern: getRandomPattern() + getRandomPattern(),
+	});
+};
+
+const clipA = scribbletune.clip({
+	notes: ROOT,
+	randomNotes: scribbletune.arp(scribbletune.getChordsByProgression(ROOT + ' ' + SCALE, 'vi v' )),
+	pattern: getRandomPattern() + getRandomPattern(),
+	subdiv: '16n'
+})
+
+const clipB = scribbletune.clip({
+	notes: ROOT,
+	randomNotes: scribbletune.arp(scribbletune.getChordsByProgression(ROOT + ' ' + SCALE, 'vi v')),
+	pattern: getRandomPattern(),
+	subdiv: '16n'
+})
+
+scribbletune.midi([].concat(clipA, clipA, clipA, clipB), 'riff.mid');
